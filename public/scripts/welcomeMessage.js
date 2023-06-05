29f1b3b649984f521c6d91d1b3b3043e808f2f3f
Juskiie <juskiehusky@icloud.com>
2023-06-05 02:11:50 +0100

    Experimental viewport changes

--- public/scripts/welcomeMessage.js
+++ public/scripts/welcomeMessage.js
@@ -1,5 +1,5 @@
 console.log("You shouldn't be here ono");
 
-export function welcomeMessageFunc() {
-	console.log("Yep. It works!");
-}
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
